283714218cc35002c53c2e385553a184f986ee72
Eric Flores <ericflorescode@gmail.com>
2018-04-06 16:18:20 -0500

    Fix for target_dir and s3_obj_path are the same

--- s3_index_generator/lib/s3lib.py
+++ s3_index_generator/lib/s3lib.py
@@ -53,15 +53,15 @@ def _search_objects(target_dir, s3_objects, s3_obj_name='', ignore_pattern=''):
         s3_obj_path = PurePosixPath(s3_obj['Key'])
         try:
             obj_rel_path = s3_obj_path.relative_to(target_dir)
-            # Needed with target_dir and s3_obj_path are the same
-            if (obj_rel_path == '.'):
-                obj_rel_path = target_dir
         except ValueError as e:
             log.trace('_search_objects s3_obj not relative_to target_dir: %s' % e.message)
             continue
 
         try:
-            obj_next_path_list = target_dir.joinpath(obj_rel_path.parts[0])
+            if (target_dir != obj_rel_path):
+                obj_next_path_list = target_dir.joinpath(obj_rel_path.parts[0])
+            else:
+                obj_next_path_list = target_dir
         except Exception as e:
             log.fatal('Error in _search_objects: "%s"' % e.message)
             log.fatal(str({
